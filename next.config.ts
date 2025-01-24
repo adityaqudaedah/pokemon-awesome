ee4c00105f6c35671bae28d8bfdd4b75b49bc980
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2025-01-24 16:58:20 +0700

    add : remote pattern

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{
-      protocol: "https",
-      hostname: "img.pokemondb.net",
-      port: "",
-      pathname: "/artwork/**",
-      search : ""
-    },{
-      protocol: "https",
-      hostname: "raw.githubusercontent.com",
-      // port: "",
-      // pathname: "/artwork/**",
-      // search : ""
-    }]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.pokemondb.net",
+        port: "",
+        pathname: "/artwork/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        // port: "",
+        // pathname: "/artwork/**",
+        // search : ""
+      },
+    ],
+  },
 };
 
 export default nextConfig;
