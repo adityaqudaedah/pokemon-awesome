0adbe4144befc8215ec3b295c839bce800785467
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2025-01-24 17:01:41 +0700

    add : types

---
+++ src/app/_libs/types/index.ts
@@ -0,0 +1,65 @@
+export type User = {
+  username: string;
+  authorized: boolean;
+};
+
+export type Pokemon = {
+  name: string;
+  url?: string;
+};
+
+export type Auth = {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+  user: User;
+};
+
+export type LoginType = {
+  username: string;
+  password: string;
+};
+
+export type PokemonList = {
+  count: number;
+  results: Array<Pokemon>;
+  next?: string;
+};
+
+type Ability = {
+  ability: { name: string };
+  is_hidden: boolean;
+};
+
+type Statistics = {
+  base_stat: number;
+  stat: { name: string };
+};
+type PokemonType = {
+  type: { name: string };
+};
+
+type Sprites = {
+  other: {
+    "official-artwork": {
+      front_default: string;
+      front_shiny: string;
+    };
+  };
+};
+
+type Move = {
+  move: { name: string };
+};
+
+export type PokemonDetailType = {
+  id: number;
+  name: string;
+  weight: number;
+  height: number;
+  abilities: Array<Ability>;
+  stats: Array<Statistics>;
+  types: Array<PokemonType>;
+  moves: Array<Move>;
+  sprites: Sprites;
+};
