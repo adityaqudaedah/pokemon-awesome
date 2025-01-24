0a3f6b28b65a431de89c4e610e6e9e2787704c79
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2025-01-24 17:02:27 +0700

    fix : progress bar

--- src/app/_components/atoms/progress-bar.tsx
+++ src/app/_components/atoms/progress-bar.tsx
@@ -3,7 +3,7 @@ const ProgressBar = ({ progress }: { progress: number }) => {
     return (
       <div className="w-full bg-gray-200 rounded-full h-4">
         <div
-          className="bg-blue-500 h-4 rounded-full"
+          className="bg-green-500 h-4 rounded-full"
           style={{ width: `${progress}%` }}
         ></div>
       </div>
